17a0d26f93281b074ab832f4aad7b55a39215d9b
Lance Shaw <AmbroseSouth@outlook.com>
2022-01-25 14:05:32 -0600

    completed

---
+++ exercise.js
@@ -0,0 +1,12 @@
+// ES2015 Global Constants
+const PI = 3.14;
+
+// QUIZ
+
+// var is global, has no scoping; also, var is hoited, and can be redeclared
+
+// var is global, has no scoping; also, var is hoited, and can be redeclared as well as reassigned
+
+// let can be reassigned.
+
+// a process by which var declarations of variables are executed first in a javascript code
